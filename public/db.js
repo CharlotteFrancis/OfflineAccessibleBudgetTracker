7cda126deb70ec269be149337f87887c6b5e64ae
Charlotte Wong <ChiharuTaru@gmail.com>
2021-07-08 09:57:34 -0700

    wrote db.js

---
+++ public/db.js
@@ -0,0 +1,45 @@
+let db
+
+const request = indexedDB.open('menu', 1)
+
+request.onupgradeneeded = ({ target }) => {
+  db = target.result
+  db.createObjectStore('pending', { autoIncrement: true })
+}
+
+request.onsuccess = ({ target }) => {
+  db = target.result
+
+  if (navigator.onLine) {
+    checkDatabase()
+  }
+}
+
+request.onerror = event => {
+  console.log(event.target.errorCode)
+}
+
+const saveRecord = record => {
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const store = transaction.objectStore('pending')
+  store.add(record)
+}
+
+const checkDatabase = _ => {
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const store = transaction.objectStore('pending')
+  const getAll = store.getAll()
+
+  getAll.onsuccess = _ => {
+    if (getAll.result.length > 0) {
+      axios.post('/api/transaction/bulk', getAll.result)
+        .then(() => {
+          const transaction = db.transaction(['pending'], 'readwrite')
+          const store = transaction.objectStore('pending')
+          store.clear()
+        })
+    }
+  }
+}
+
+window.addEventListener('online', checkDatabase)
